18b29110531f2963e765fa4d5b1b1e5e50f7c38e
Wandavisionv3 <101384551+Wandavisionv3@users.noreply.github.com>
2022-03-11 11:59:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -108,7 +108,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}]🦋 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -748,7 +748,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="𝓛𝓸𝓪𝓭𝓲𝓷𝓰"
         )
         await query.message.edit_text(
-            text="ᎳᎪᏁᎠᎪᏉᎥsᎥᎾᏁ"
+            text="ᔑᑕᗩᖇᏞᗴᎢ ᗯᏆᎢᑕᕼ 🦹‍♀"
         )
         await query.message.edit_text(        
             text="Select your required mode from below!",
@@ -761,35 +761,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁🦜', callback_data='autofilter'),
             InlineKeyboardButton('𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙸𝙾𝙽𝚂🪶', callback_data='coct')
             ],[
-            InlineKeyboardButton('𝚂𝙾𝙽𝙶🌷', callback_data='songs'),
+            InlineKeyboardButton('𝚂𝙾𝙽𝙶🎶', callback_data='songs'),
             InlineKeyboardButton('𝙴𝚇𝚃𝚁𝙰💮', callback_data='extra'),
             InlineKeyboardButton("𝚅𝙸𝙳𝙴𝙾🦭", callback_data='video')
             ],[
-            InlineKeyboardButton('𝙿𝙸𝙽🐡', callback_data='pin'), 
-            InlineKeyboardButton('𝙿𝙰𝚂𝚃𝙴🐠', callback_data='pastes'),
-            InlineKeyboardButton("𝙸𝙼𝙰𝙶𝙴", callback_data='image')
-            ],[
-            InlineKeyboardButton('𝙵𝚄𝙽🐲', callback_data='fun'), 
-            InlineKeyboardButton('𝙹𝚂𝙾𝙽𝙴🐍', callback_data='son'),
-            InlineKeyboardButton('𝚃𝚃𝚂🦉', callback_data='ttss')
-            ],[
-            InlineKeyboardButton('𝙿𝚄𝚁𝙶𝙴🐦', callback_data='purges'),
-            InlineKeyboardButton('𝙿𝙸𝙽𝙶🦩', callback_data='pings'),
-            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷🕷', callback_data='tele')
-            ],[
             InlineKeyboardButton('𝚆𝙷𝙾𝙸𝚂🦚', callback_data='whois'),
             InlineKeyboardButton('𝙼𝚄𝚃𝙴🐝', callback_data='restric'),
-            InlineKeyboardButton('𝙺𝙸𝙲𝙺🌹', callback_data='zombies')
+            InlineKeyboardButton('𝙺𝙸𝙲𝙺🥊', callback_data='zombies')
             ],[
             InlineKeyboardButton('𝚁𝙴𝙿𝙾𝚁𝚃⚘', callback_data='report'),
-            InlineKeyboardButton('𝚈𝚃-𝚃𝙷𝚄𝙼𝙱🌷', callback_data='ytthumb'),
-            InlineKeyboardButton('𝚂𝚃𝙸𝙲𝙺𝙴𝚁-𝙸𝙳🌻', callback_data='sticker')
-            ],[
-            InlineKeyboardButton('𝙲𝙾𝚅𝙸𝙳🌾', callback_data='corona'),
-            InlineKeyboardButton('𝙰𝚄𝙳𝙸𝙾-𝙱𝙾𝙾𝙺🌿', callback_data='abook'),
-            InlineKeyboardButton('𝚄𝚁𝙻-𝚂𝙷𝙾𝚁𝚃🍃', callback_data='urlshort')
-            ],[
-            InlineKeyboardButton('𝙶-𝚃𝚁𝙰𝙽𝚂🌺', callback_data='gtrans'),
             InlineKeyboardButton('𝙵𝙸𝙻𝙴-𝚂𝚃𝙾𝚁𝙴🦋', callback_data='newdata'),
             InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚄𝚂🦁', callback_data='stats')
             ],[
@@ -818,7 +798,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="𝓛𝓸𝓪𝓭𝓲𝓷𝓰"
         )
         await query.message.edit_text(
-            text="ᎳᎪᏁᎠᎪᏉᎥsᎥᎾᏁ"
+            text="ᔑᑕᗩᖇᏞᗴᎢ ᗯᏆᎢᑕᕼ 🦹‍♀"
         )
         await query.message.edit_text(          
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -827,7 +807,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕', url='https://t.me/+AMHw_K1wvOM3MTU9'),
+            InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕', url='https://t.me/Newcinemazz),
             InlineKeyboardButton(' 𝚂𝚘𝚞𝚛𝚌𝚎', callback_data='source')
             ],[
             InlineKeyboardButton(' 𝙷𝚘𝚖𝚎', callback_data='start'),
@@ -856,7 +836,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="𝓛𝓸𝓪𝓭𝓲𝓷𝓰"
         )
         await query.message.edit_text(
-            text="ᎳᎪᏁᎠᎪᏉᎥsᎥᎾᏁ"
+            text="ᔑᑕᗩᖇᏞᗴᎢ ᗯᏆᎢᑕᕼ 🦹‍♀"
         )
         await query.message.edit_text(          
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -1283,7 +1263,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}]🦋 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
